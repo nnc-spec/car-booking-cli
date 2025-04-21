cce5cb4d53e06294c8549d5b40573a67dbabe403
nese <naziknesecindik@gmail.com>
2025-04-21 23:46:16 +0200

    Refactoring to use Streams.

--- src/main/java/car/CarService.java
+++ src/main/java/car/CarService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarService {
     private final CarDao carDao;
@@ -26,22 +27,10 @@ public class CarService {
     }
 
     public List<Car> getElectricCars(){
-        int electricCarsCount = 0;
-
-        List<Car> cars = getAllCars();
-
-        if(cars.size()== 0){
-            return Collections.emptyList();
-        }
-
-        List<Car> electricCars = new ArrayList<>();
-
-        for(Car car: cars){
-            if(car.isElectric()){
-               electricCars.add(car);
-            }
-        }
-
+        List<Car> electricCars = getAllCars()
+                .stream()
+                .filter(car -> car.isElectric())
+                .collect(Collectors.toList());
         return electricCars;
     }
 }
